8c2e298ba9af1e9e383ee1db8ccf1f4361b6f6e3
francesco1648 <148702524+francesco1648@users.noreply.github.com>
2025-04-11 12:09:46 +0200

    va

--- build/sketch/dynamixel_test.ino.cpp
+++ build/sketch/dynamixel_test.ino.cpp
@@ -4,12 +4,15 @@
 
 DynamixelLL motor1(Serial1, 1);  // ID = 1
 DynamixelLL motor2(Serial1, 2);  // ID = 2
+int pos1 = 2047; // Posizione iniziale del motore 1
+int pos2 = 2048; // Posizione iniziale del motore 2
+int step = 50;  // Incremento/decremento per ogni pressione del tasto
 
-#line 6 "C:\\Users\\Titania\\Desktop\\isaac\\test\\dynamixel_test\\dynamixel_test.ino"
+#line 9 "C:\\Users\\Titania\\Desktop\\isaac\\test\\dynamixel_test\\dynamixel_test.ino"
 void setup();
-#line 27 "C:\\Users\\Titania\\Desktop\\isaac\\test\\dynamixel_test\\dynamixel_test.ino"
+#line 33 "C:\\Users\\Titania\\Desktop\\isaac\\test\\dynamixel_test\\dynamixel_test.ino"
 void loop();
-#line 6 "C:\\Users\\Titania\\Desktop\\isaac\\test\\dynamixel_test\\dynamixel_test.ino"
+#line 9 "C:\\Users\\Titania\\Desktop\\isaac\\test\\dynamixel_test\\dynamixel_test.ino"
 void setup() {
   Serial.begin(115200);
   while (!Serial);
@@ -29,14 +32,91 @@ void setup() {
   motor1.writeRegister(64, 1, 1);  // Address 65, Value 1, Size 1 byte
   motor2.writeRegister(64, 1, 1);  // Address 65, Value 1, Size 1 byte
   delay(1000);
+  motor1.writeRegister(116, pos1, 4);  // Address 65, Value 1, Size 1 byte
+  motor2.writeRegister(116, pos2, 4);  // Address 65, Value 1, Size 1 byte
+  delay(1000);
 }
 
 void loop() {
-  motor1.writeRegister(116, 2000, 4);  // Address 65, Value 1, Size 1 byte
-  motor2.writeRegister(116, 2000, 4);  // Address 65, Value 1, Size 1 byte
-  delay(3000);
-  motor1.writeRegister(116, 0, 4);  // Address 65, Value 1, Size 1 byte
-  motor2.writeRegister(116, 0, 4);  // Address 65, Value 1, Size 1 byte
-  delay(3000);
+  if (Serial.available()>0)
+  {
+
+    char c = Serial.read();
+
+       switch (c)
+       {
+       case 'w': // W -> Motore 1 avanti
+       case 'W':
+pos1 += step; // Incrementa la posizione del motore 1
+pos2 -= step; // Incrementa la posizione del motore 2
+  motor1.writeRegister(116, pos1, 4);  // Address 65, Value 1, Size 1 byte
+  motor2.writeRegister(116, pos2, 4);  // Address 65, Value 1, Size 1 byte
+         Serial.print("Motore 1 posizione: ");
+         Serial.print(pos1);
+         Serial.print("\t");
+         Serial.print("Motore 2 posizione: ");
+         Serial.println(pos2);
+         break;
+
+       case 's': // S -> Motore 1 indietro
+       case 'S':
+pos1 -= step; // Decrementa la posizione del motore 1
+pos2 += step; // Decrementa la posizione del motore 2
+
+  motor1.writeRegister(116, pos1, 4);  // Address 65, Value 1, Size 1 byte
+  motor2.writeRegister(116, pos2, 4);  // Address 65, Value 1, Size 1 byte
+       Serial.print("Motore 1 posizione: ");
+         Serial.print(pos1);
+         Serial.print("\t");
+         Serial.print("Motore 2 posizione: ");
+         Serial.println(pos2);
+         break;
+
+       case 'd': // D -> Motore 2 avanti
+       case 'D':
+pos1 -= step; // Incrementa la posizione del motore 1
+pos2 -= step; // Incrementa la posizione del motore 2
+
+  motor1.writeRegister(116, pos1, 4);  // Address 65, Value 1, Size 1 byte
+  motor2.writeRegister(116, pos2, 4);  // Address 65, Value 1, Size 1 byte
+         Serial.print("Motore 1 posizione: ");
+         Serial.print(pos1);
+         Serial.print("\t");
+         Serial.print("Motore 2 posizione: ");
+         Serial.println(pos2);
+         break;
+
+       case 'a': // A -> Motore 2 indietro
+       case 'A':
+pos1 += step; // Decrementa la posizione del motore 1
+pos2 += step; // Decrementa la posizione del motore 2
+
+       motor1.writeRegister(116, pos1, 4);  // Address 65, Value 1, Size 1 byte
+       motor2.writeRegister(116, pos2, 4);  // Address 65, Value 1, Size 1 byte
+       Serial.print("Motore 1 posizione: ");
+         Serial.print(pos1);
+         Serial.print("\t");
+         Serial.print("Motore 2 posizione: ");
+         Serial.println(pos2);
+         break;
+
+       default:
+         Serial.println("Tasto non riconosciuto.");
+         break;
+         delay(10);
+       }
+
+       while (Serial.available())
+         Serial.read(); // Pulisce il buffer
+         delay(10);
+     }
+     delay(10);
+
+
+
+
+
+
+
 }
 
